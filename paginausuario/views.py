2853a633480e92c70b2ad94e26f7008cb894667a
carlosdgd <robertocarlosalv2006@gmail.com>
2025-12-13 16:35:57 -0500

    second commit

--- paginausuario/views.py
+++ paginausuario/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def paginabienvenida(request):
-    titulo= "Curioso"
+    titulo= "jorge el Curioso"
 
     return render(request, 'signup.html',{'mytitle':titulo})
