86e92b4ab4575f0f81de32d32141880342cf9567
carlosdgd <robertocarlosalv2006@gmail.com>
2025-12-13 20:27:21 -0500

    second commit

--- hojadevida/settings.py
+++ hojadevida/settings.py
@@ -122,7 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
+
 if not DEBUG:
-    STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
+    
     STATICFILES_STORAGE ='whitenoise.storage.CompressedManifestStaticFilesStorage'
